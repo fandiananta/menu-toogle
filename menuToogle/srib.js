17cca94f309f81a98ba5b7ce59c7056c75ea370a
FANDI ANANTA <68496408+fandiananta@users.noreply.github.com>
2020-07-21 09:41:57 +0700

    Add files via upload

---
+++ menuToogle/srib.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('tombol');
+
+const nav = document.querySelector('ul');
+
+
+button.addEventListener('click', function() {
+    // muncol.classList.toogle('slide');
+    nav.classList.toggle('slide');
+});
